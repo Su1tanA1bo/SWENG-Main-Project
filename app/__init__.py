a029c63cb4bcdf0fce829f3447660ed53543e389
Indigo Bosworth <indigobosworth@gmail.com>
2022-11-15 15:40:39 +0000

    added flask babel configuration

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,7 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel
+from flask_babel import lazy_gettext as _l
 
 #necessary code for app initialisation
 app = Flask(__name__)
@@ -27,6 +28,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
