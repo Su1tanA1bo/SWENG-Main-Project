8085c69cfa3cce9544536116eacaf3fbb0787680
Indigo Bosworth <indigobosworth@gmail.com>
2022-11-15 16:14:02 +0000

    added use of bp to handlers.py

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -8,15 +8,18 @@
 ##*************************************************************************
 
 from flask import render_template
-from app import app, db
+from app import db
+from app.errors import bp
+
 
 #function handles error of type 404
-@app.errorhandler(404)
+@bp.app_errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
+
 
 #function handles error of type 500
-@app.errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('errors/500.html'), 500
