6a124cfd0a0c2e61e890c46d9d770a2beb5131bd
taylorj8 <taylorj8@tcd.ie>
2022-11-17 21:10:32 +0000

    working on getting data about commit sizes

--- api.py
+++ api.py
@@ -5,22 +5,29 @@
 #   @Creation Date: 16/11/2022
 ##*************************************************************************
 
+from commit import Commit
 from pprint import pprint
 from requests import get
 from user import User
 
 
-def api_fetch():
+def api_fetch(ref):
     username = "taylorj8"
     repo = "protocol"
     auth = "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"
+    queries = "?per_page=20"
+    if not ref:
+        commit = ""
+    else:
+        commit = "/" + ref
 
     headers = {
         "Authorization": "token " + auth,
         "Accept": "application/vnd.github+json"
     }
 
-    url = f"https://api.github.com/repos/{username}/{repo}/commits?&per_page=100"
+    # if ref specified as input, searches for specific commit, otherwise lists all commits
+    url = f"https://api.github.com/repos/{username}/{repo}/commits{commit}{queries}"
     response = get(url, headers=headers).json()
     return response
 
@@ -33,11 +40,21 @@ def commit_info(json):
         day = date[:10]
         time = date[11:-1]
 
-        if name in commits:
-            commits[name].add_commit(day, time)
-        else:
-            commits[name] = User(name, day, time)
+        sha = entry["sha"]
+        commit_json = api_fetch(sha)
+        message = commit_json["commit"]["message"]
+        changes = commit_json["stats"]
+        commit = Commit(sha, message, changes["additions"], changes["deletions"])
 
+        if name in commits:
+            commits[name].add_commit(day, time, commit)
+        else:
+            commits[name] = User(name, day, time, commit)
+
+    print_stats(commits)
+
+
+def print_stats(commits):
     for name in commits:
         commits[name].resolve_stats()
         print(f"User: {name}\n"
@@ -45,10 +62,12 @@ def commit_info(json):
               f"Days committed: {commits[name].days_committed}\n"
               f"Most commits: {commits[name].most_commits[1]} on {commits[name].most_commits[0]}\n"
               f"Least commits: {commits[name].least_commits[1]} on {commits[name].least_commits[0]}\n"
-              f"Average frequency: {commits[name].avg_freq} commits per day")
+              f"Average frequency: {commits[name].avg_freq} commits per day\n"
+              f"Largest commit: {commits[name].most_changes}")
         commits[name].print_commits()
 
 
 if __name__ == '__main__':
-    response = api_fetch()
+    response = api_fetch(False)
+    # pprint(response)
     commit_info(response)
--- user.py
+++ user.py
@@ -8,40 +8,73 @@
 from pprint import pprint
 
 
+def resolve_changes(commits, ref):
+    most = (0, None)
+    least = (float('inf'), None)
+
+    # if self.commits[day][index] > most:
+    #     most = self.commits[day].
+    # if self.commits[day][index] < least:
+    #     least = self.commits[day][index]
+
+    for commit in commits:
+        if commit.total_changes > most[0]:
+            most = (commit.total_changes, commit)
+        if commit.total_changes < least[0]:
+            least = (commit.total_changes, commit)
+
+    return most, least
+
+
 class User:
 
-    def __init__(self, name, day, time):
+    def __init__(self, name, day, time, commit):
         self.name = name
         self.commits = {}
         self.total_commits = 0
-        self.add_commit(day, time)
+        self.add_commit(day, time, commit)
 
         self.days_committed = None
         self.avg_freq = -1
         self.most_commits = -1
         self.least_commits = -1
 
-    def add_commit(self, day, time):
+        self.most_additions = (-1, None)
+        self.least_additions = (-1, None)
+        self.most_deletions = (-1, None)
+        self.least_deletions = (-1, None)
+        self.most_changes = (-1, None)
+        self.least_changes = (-1, None)
+
+    def add_commit(self, day, time, commit):
         if day in self.commits:
-            self.commits[day].append(time)
+            self.commits[day].append({time: commit})
         else:
-            self.commits[day] = [time]
+            self.commits[day] = [{time: commit}]
         self.total_commits += 1
 
     def resolve_stats(self):
         self.days_committed = len(self.commits)
         self.avg_freq = round(self.total_commits / self.days_committed)
 
-        most = 0
-        least = float('inf')
+        most_commits = 0
+        least_commits = float('inf')
 
         for day in self.commits:
-            if len(self.commits[day]) > most:
-                most = len(self.commits[day])
-                self.most_commits = (day, most)
-            if len(self.commits[day]) < least:
-                least = len(self.commits[day])
-                self.least_commits = (day, least)
+            if len(self.commits[day]) > most_commits:
+                most_commits = len(self.commits[day])
+                self.most_commits = (day, most_commits)
+
+            if len(self.commits[day]) < least_commits:
+                least_commits = len(self.commits[day])
+                self.least_commits = (day, least_commits)
+
+            # self.most_additions, self.least_additions = self.resolve_changes(day, "additions")
+            # self.most_deletions, self.least_deletions = self.resolve_changes(day, "deletions")
+            self.most_changes, self.least_changes = resolve_changes(self.commits[day], "changes")
+
+    def return_commit_message(self, day, time):
+        return self.commits[day][time]["Message"]
 
     def print_commits(self):
         pprint(self.commits)
