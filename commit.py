d8a74a4c4425f06a5a02312c307fcceb2419a436
taylorj8 <taylorj8@tcd.ie>
2022-11-17 19:15:40 +0000

    added commit class to store data about commits

---
+++ commit.py
@@ -0,0 +1,22 @@
+##*************************************************************************
+#   data structure containing all relevant data about a commit
+#
+#   @author	 Jamie Taylor
+#   @Creation Date: 17/11/2022
+##*************************************************************************
+
+
+class Commit:
+
+    def __init__(self, sha, message, additions, deletions):
+        self.sha = sha
+        self.message = message
+        self.additions = additions
+        self.deletions = deletions
+        self.total_changes = additions + deletions
+
+    def __repr__(self):
+        return f"Message: {self.message}\n" \
+               f"Sha: {self.sha}\n" \
+               f"Additions: {self.additions}\n" \
+               f"Deletions: {self.deletions}"
