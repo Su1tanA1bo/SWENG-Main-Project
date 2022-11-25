38a14c68bd55917b5acb3c5f3d8426cefeb280e2
gutsteip <gutsteip@tcd.ie>
2022-11-25 10:22:14 +0000

    Updated UserStats method
    Added methods to UserStats

--- app/models.py
+++ app/models.py
@@ -176,8 +176,103 @@ class UserStats(db.Model):
         self.avg_no_deletions = -1
         self.avg_no_changes = -1
 
-        #lists and dicts in the user.py need to become tables in models.py
+    # add a commit to commits list
+    def add(self, commit):
+        self.commits.append(commit)
 
+    # finds the number of days that a user committed on
+    def find_days_committed(self):
+        days = set()
+        for commit in self.commits:
+            days.add(commit.date)
+        return len(days)
+
+    # function for adding number of lines written
+    def add_to_lines(self, lines):
+        self.lines_written += lines
+
+    # calculate percentage of code owner by this user
+    def calculate_code_ownership(self, total_lines):
+        self.code_ownership = round((self.lines_written / total_lines) * 100, 3)
+
+    # getter for total number of commits
+    def total_commits(self):
+        return len(self.commits)
+
+    # pretty prints the list of commits
+    def print_commits(self):
+        pprint(self.commits)
+        print("\n")
+
+    # update all the relevant fields for a user
+    def resolve_stats(self):
+        self.days_committed = self.find_days_committed()
+        self.avg_freq = round(len(self.commits) / self.days_committed)
+
+        most_changes = 0
+        least_changes = float('inf')
+
+        most_additions = 0
+        least_additions = float('inf')
+
+        most_deletions = 0
+        least_deletions = float('inf')
+
+        total_changes = 0
+        total_additions = 0
+        total_deletions = 0
+
+        # dict with date as key, number of commits as value
+        commits_per_day = {}
+        for commit in self.commits:
+            total_changes += commit.changes
+            total_additions += commit.additions
+            total_deletions += commit.deletions
+
+            # following ifs compare current commit with current biggest/smallest
+            if commit.changes > most_changes:
+                most_changes = commit.changes
+                self.most_changes = (most_changes, commit)
+            if commit.changes < least_changes:
+                least_changes = commit.changes
+                self.least_changes = (least_changes, commit)
+
+            if commit.additions > most_additions:
+                most_additions = commit.additions
+                self.most_additions = (most_additions, commit)
+            if commit.additions < least_additions:
+                least_additions = commit.additions
+                self.least_additions = (least_additions, commit)
+
+            if commit.deletions > most_deletions:
+                most_deletions = commit.deletions
+                self.most_deletions = (most_deletions, commit)
+            if commit.deletions < least_deletions:
+                least_deletions = commit.deletions
+                self.least_deletions = (least_deletions, commit)
+
+            # counts commits in each day
+            if commit.date in commits_per_day:
+                commits_per_day[commit.date] += 1
+            else:
+                commits_per_day[commit.date] = 1
+
+        most_commits = 0
+        least_commits = float('inf')
+
+        # finds the days with the most and least commits
+        for day in commits_per_day:
+            if commits_per_day[day] > most_commits:
+                most_commits = commits_per_day[day]
+                self.most_commits = (day, most_commits)
+            if commits_per_day[day] < least_commits:
+                least_commits = commits_per_day[day]
+                self.least_commits = (day, least_commits)
+
+        # gets averages
+        self.avg_no_changes = round(total_changes / len(self.commits))
+        self.avg_no_additions = round(total_additions / len(self.commits))
+        self.avg_no_deletions = round(total_deletions / len(self.commits))
 
 #Class for commit in database. Works as a tuple in practie, as tuples cannot be stored in db naturally
 class Commit(db.Model):
